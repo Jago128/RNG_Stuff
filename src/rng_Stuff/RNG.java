e428ec486fa2c959ac39817e94a900fc8acd91cc
Jago128 <jagobaetxegarai@gmail.com>
2024-12-31 19:44:40 +0100

    RNG Stuff

--- src/rng_Stuff/RNG.java
+++ src/rng_Stuff/RNG.java
@@ -4,11 +4,21 @@ import java.util.Random;
 import utilidades.Utilidades;
 public class RNG {
 
-	public static int menu() {
-		System.out.println("1. Tem Armor");
-		System.out.println("2. Other RNG");
-		System.out.println("RNG choice:");
-		return Utilidades.leerInt(1,2);
+	public static void main(String[] args) {
+		Random a= new Random();
+		int menu;
+		do {
+			switch (menu=menu()) {
+
+			case 1:
+				temArmor(a);
+				break;
+
+			case 2:
+				otherRNG(a);
+				break;
+			}
+		} while (menu!=1&&menu!=2);
 	}
 	
 	public static void temArmor(Random a) {
@@ -23,30 +33,27 @@ public class RNG {
 			System.out.println(rng);
 		}
 	}
-	
+
 	public static void otherRNG(Random a) {
-		int rng=a.nextInt(4);
-		if (rng==0) {
-			System.out.println("The roll landed on 0.");
-			rng++;
-		}
-		System.out.println(rng);
-	}
-	
-	public static void main(String[] args) {
-		Random a= new Random();
-		int menu;
-		do {
-			switch (menu=menu()) {
-			
-			case 1:
-				temArmor(a);
-				break;
-				
-			case 2:
-				otherRNG(a);
-				break;
+		int num, times;
+		System.out.println("Number:");
+		num=Utilidades.leerInt();
+		System.out.println("Times:");
+		times=Utilidades.leerInt();
+		for (int i=0;i<times;i++) {
+			int rng=a.nextInt(num+1);
+			if (rng==0) {
+				System.out.println("RNG roll landed at 0.");
+				rng++;
 			}
-		} while (menu!=1&&menu!=2);
+			System.out.println(rng);
+		}
+	}
+
+	public static int menu() {
+		System.out.println("1. Tem Armor");
+		System.out.println("2. Other RNG");
+		System.out.println("RNG choice:");
+		return Utilidades.leerInt(1,2);
 	}
 }
