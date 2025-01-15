11f350f2052472632f3b732a60605a118e8e3188
Jago128 <jagobaetxegarai@gmail.com>
2025-01-15 21:39:11 +0100

    e

--- src/rng_Stuff/RNG.java
+++ src/rng_Stuff/RNG.java
@@ -7,22 +7,32 @@ public class RNG {
 	public static void main(String[] args) {
 		Random a= new Random();
 		int menu=menu();
-		do {
-			switch (menu) {
 
-			case 1:
-				temArmor(a);
-				break;
+		switch (menu) {
+		case 1:
+			temArmor(a);
+			break;
 
-			case 2:
-				otherRNG(a);
-				break;
-			}
-		} while (menu!=1&&menu!=2);
+		case 2:
+			otherRNG(a);
+			break;
+
+		case 3:
+			conchSlasherRNG(a);
+			break;
+
+		case 4:
+			conchDestroyerRNG(a);
+			break;
+
+		case 0:
+			System.out.println("Program end.");
+		}
 	}
-	
+
 	public static void temArmor(Random a) {
-		System.out.println("Specify the hit count (Tem Armor DMG Heal RNG):");
+		System.out.println("Temmie Armor Damage Heal. 10% of triggering");
+		System.out.println("Specify the hit count:");
 		int times=Utilidades.leerInt();
 		for (int i=0;i<times;i++) {
 			int rng=a.nextInt(11);
@@ -50,10 +60,41 @@ public class RNG {
 		}
 	}
 
+	public static void conchSlasherRNG(Random a) {
+		int times;
+		System.out.println("Conch Slasher. 10% chance of critting.");
+		System.out.println("Attack count:");
+		times=Utilidades.leerInt();
+		for (int i=0;i<times;i++) {
+			int rng=a.nextInt(11);
+			if (rng==0) {
+				System.out.println("RNG roll landed at 0.");
+				rng++;
+			}
+			System.out.println(rng);
+		}
+	}
+
+	public static void conchDestroyerRNG(Random a) {
+		int times;
+		System.out.println("Conch Destroyer. 20% chance of critting.");
+		System.out.println("Attack count:");
+		times=Utilidades.leerInt();
+		for (int i=0;i<times;i++) {
+			int rng=a.nextInt(6);
+			if (rng==0) {
+				System.out.println("RNG roll landed at 0.");
+				rng++;
+			}
+			System.out.println(rng);
+		}
+	}
+
 	public static int menu() {
 		System.out.println("1. Tem Armor");
 		System.out.println("2. Other RNG");
-		System.out.println("RNG choice:");
-		return Utilidades.leerInt(1,2);
+		System.out.println("3. Conch Slasher Crit RNG");
+		System.out.println("4. Conch Destroyer Crit RNG");
+		return Utilidades.leerInt("RNG choice:",1,4);
 	}
 }
