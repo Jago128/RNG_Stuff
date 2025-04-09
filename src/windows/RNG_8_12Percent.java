9e333db6e3f027951c028538c8cde9b9d9d4db3f
Jago128 <jagobaetxegarai@gmail.com>
2025-04-09 09:38:21 +0200

    Empied layouts

--- src/windows/RNG_8_12Percent.java
+++ src/windows/RNG_8_12Percent.java
@@ -12,25 +12,8 @@ public class RNG_8_12Percent extends JDialog {
 	public RNG_8_12Percent() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
+		contentPanel.setLayout(null);
 	}
-
 }
