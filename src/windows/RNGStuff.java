f16d06f6a65b499d0bd04c8ec5c492b0730be217
Jago128 <jagobaetxegarai@gmail.com>
2025-04-26 14:07:06 +0200

    Centered the title

--- src/windows/RNGStuff.java
+++ src/windows/RNGStuff.java
@@ -60,7 +60,7 @@ public class RNGStuff extends JFrame implements ActionListener {
 		JLabel lblTitle = new JLabel("Jago's RP/Parody RNG Shenanigans");
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitle.setFont(new Font("Determination Mono Web", Font.PLAIN, 24));
-		lblTitle.setBounds(36, 10, 401, 41);
+		lblTitle.setBounds(151, 10, 401, 41);
 		contentPane.add(lblTitle);
 
 		btnJustitiaRNG = new JButton("Justitia RNG");
