e5314c3498cd380d89bb4fb43997ac61a615872b
Jago128 <jagobaetxegarai@gmail.com>
2026-03-05 09:15:50 +0100

    Here we go again.

---
+++ src/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/view/MainWindow.fxml"));
+			Scene scene = new Scene(root);
+			stage.setTitle("Main Window");
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
